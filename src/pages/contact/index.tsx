dcde58dc92812e349e848fac2e9f6617a732a5ae
piro <piro.haniwa@gmail.com>
2022-02-21 03:06:37 +0900

    fix: add submit handler

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,11 +1,16 @@
-import ContactTop from "components/ContactTop";
+import { useCallback } from "react";
+import ContactTop, { ContactTopProps } from "components/ContactTop";
 import Seo from "components/Seo";
 
 function Contact(): JSX.Element {
+  const handleSubmit = useCallback<ContactTopProps["onSubmit"]>((data) => {
+    console.log(data);
+  }, []);
+
   return (
     <>
       <Seo title="CONTACT" />
-      <ContactTop />
+      <ContactTop onSubmit={handleSubmit} />
     </>
   );
 }
